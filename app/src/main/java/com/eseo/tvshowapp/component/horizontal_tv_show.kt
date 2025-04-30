8f7a9381aaef574c37b242d8a3b9c3421b977d89
axel-bis <naxelcdric@gmail.com>
2025-04-30 17:32:32 +0200

    Added horizontal_tv_show.kt

---
+++ app/src/main/java/com/eseo/tvshowapp/component/horizontal_tv_show.kt
@@ -0,0 +1,47 @@
+package com.eseo.tvshowapp.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.eseo.tvshowapp.R
+import com.eseo.tvshowapp.data.tvShows
+
+
+@Composable
+fun HorizontalTvShow(index: Int) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+    ) {
+        // Il faut un Layout
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.ic_launcher_background
+                ),
+                contentDescription = null
+            )
+
+            Text(
+                text = tvShows[index].title,
+                fontSize = 20.sp
+            )
+        }
+    }
+}
--- app/src/main/java/com/eseo/tvshowapp/screen/home_screen.kt
+++ app/src/main/java/com/eseo/tvshowapp/screen/home_screen.kt
@@ -1,17 +1,26 @@
 package com.eseo.tvshowapp.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.eseo.tvshowapp.R
+import com.eseo.tvshowapp.component.HorizontalTvShow
+import com.eseo.tvshowapp.data.tvShows
 import com.eseo.tvshowapp.ui.theme.PurpleGrey40
 
 @Preview
@@ -26,15 +35,10 @@ fun HomeScreen(modifier: Modifier = Modifier) {
         ),
         contentPadding = PaddingValues(20.dp)
     ) {
-        items(10) {
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(20.dp)
-            ) {  }
+        items(tvShows.size) { index ->
+            HorizontalTvShow(index)
         }
     }
-
 }
 
 
